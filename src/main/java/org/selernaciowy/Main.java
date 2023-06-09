02324f3bbd2d241080a7ca49e3e2f37b13fd9827
Mateusz Biel <matbiel@gmail.com>
2023-06-09 10:34:48 +0200

    Add slf4j and reload4j

--- src/main/java/org/selernaciowy/Main.java
+++ src/main/java/org/selernaciowy/Main.java
@@ -1,12 +1,15 @@
 package org.selernaciowy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(SelerNaciowyConfiguration.class);
         AppFacade facade = context.getBean(AppFacade.class);
+        log.info("Log log");
         facade.run();
     }
 }
