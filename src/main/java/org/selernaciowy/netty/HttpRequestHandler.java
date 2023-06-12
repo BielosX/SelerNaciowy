a83c1c98bff3724209b8cbfe68ba1184b72c4e4a
Mateusz Biel <matbiel@gmail.com>
2023-06-12 21:16:25 +0200

    Handle void return type and 404

--- src/main/java/org/selernaciowy/netty/HttpRequestHandler.java
+++ src/main/java/org/selernaciowy/netty/HttpRequestHandler.java
@@ -1,5 +1,6 @@
 package org.selernaciowy.netty;
 
+import com.google.gson.Gson;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
@@ -13,6 +14,7 @@ import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 import io.netty.util.ReferenceCountUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +31,7 @@ import java.util.Map;
 @ChannelHandler.Sharable
 public class HttpRequestHandler extends ChannelInboundHandlerAdapter {
     private final HttpPathSegment root = new HttpPathSegment();
+    private final Gson gson = new Gson();
     private final static Map<HttpMethod, HttpRequestMethod> methodsMapping = Map.of(
             HttpMethod.GET, HttpRequestMethod.GET,
             HttpMethod.POST, HttpRequestMethod.POST
@@ -47,27 +50,47 @@ public class HttpRequestHandler extends ChannelInboundHandlerAdapter {
     }
 
     @SneakyThrows
-    private static void invoke(HttpPathSegment.InvokerAndMethod invokerAndMethod) {
-        invokerAndMethod.method().invoke(invokerAndMethod.invoker());
+    private static Object invoke(HttpPathSegment.InvokerAndMethod invokerAndMethod) {
+        return invokerAndMethod.method().invoke(invokerAndMethod.invoker());
+    }
+
+    private static FullHttpResponse notFound(HttpVersion version) {
+        return new DefaultFullHttpResponse(version, HttpResponseStatus.NOT_FOUND);
+    }
+
+    private static FullHttpResponse emptyResponse(HttpVersion version) {
+        return new DefaultFullHttpResponse(version, HttpResponseStatus.NO_CONTENT);
+    }
+
+    private static FullHttpResponse withBody(HttpVersion version, String body) {
+        byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
+        FullHttpResponse response = new DefaultFullHttpResponse(version,
+                HttpResponseStatus.OK,
+                Unpooled.wrappedBuffer(bodyBytes));
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON);
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, bodyBytes.length);
+        return response;
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         if (msg instanceof FullHttpRequest request) {
-            String responseString = "Hello";
             List<String> segments = Arrays.stream(request.uri().split("/"))
                     .filter(str -> !str.isBlank())
                     .toList();
             HttpRequestMethod requestMethod = methodsMapping.get(request.method());
-            root.findMapping(requestMethod, segments)
-                    .ifPresent(HttpRequestHandler::invoke);
-            FullHttpResponse response = new DefaultFullHttpResponse(request.protocolVersion(),
-                    HttpResponseStatus.OK,
-                    Unpooled.wrappedBuffer(responseString.getBytes(StandardCharsets.UTF_8)));
-            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.CLOSE);
-            response.headers().set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.TEXT_PLAIN);
-            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
-            ChannelFuture future = ctx.write(response);
+            ChannelFuture future = root.findMapping(requestMethod, segments)
+                    .map(invokerAndMethod -> {
+                        Object result = invoke(invokerAndMethod);
+                        Class<?> returnType = invokerAndMethod.method().getReturnType();
+                        if (returnType.equals(void.class) || returnType.equals(Void.class)) {
+                            return ctx.write(emptyResponse(request.protocolVersion()));
+                        } else {
+                            String responseString = gson.toJson(result);
+                            return ctx.write(withBody(request.protocolVersion(), responseString));
+                        }
+                    })
+                    .orElseGet(() -> ctx.write(notFound(request.protocolVersion())));
             future.addListener(ChannelFutureListener.CLOSE);
             ReferenceCountUtil.release(msg);
         } else {
